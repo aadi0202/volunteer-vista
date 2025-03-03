8e58a360a5e888bd18413646790099324d2e23f0
Aaditya Dharmen Punatar <aadipunatar@gmail.com>
2025-03-03 13:12:39 +0530

    Update app.py to include jsonify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
